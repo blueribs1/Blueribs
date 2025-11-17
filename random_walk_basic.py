80afd07b19b12a0eec9b8c633b283baa47a31b5b
blueribs <2177n2600w@gmail.com>
2025-11-17 11:32:15 -0700

    random_walk_basic

---
+++ random_walk_basic.py
@@ -0,0 +1,33 @@
+def main():
+    import matplotlib.pyplot as plt
+    import random
+    x_pos = 0
+    y_pos = 0
+    x = [0]
+    y = [0]
+    z = [zip(x, y)]
+    a = 101
+    for b in range(a):
+        plt.plot(x, y, 'o-')
+        rand = random.randint(0,3)
+        if rand == 0:
+            x_pos-=1
+            x.append(x_pos)
+            y.append(y_pos)
+        if rand == 1:
+            x_pos+=1
+            x.append(x_pos)
+            y.append(y_pos)
+        if rand == 2:
+            y_pos-=1
+            x.append(x_pos)
+            y.append(y_pos)
+        if rand == 3:
+            y_pos+=1
+            x.append(x_pos)
+            y.append(y_pos)
+    plt.xlabel("X-axis")
+    plt.ylabel("Y-axis")
+    plt.title("Random Walk")
+    plt.show()
+main()
