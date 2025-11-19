3bf27eb8668f2762206fe1fbff0f894d7eb2ec26
blueribs <2177n2600w@gmail.com>
2025-11-19 13:34:58 -0700

    random_walk_basic

--- random_walk_basic.py
+++ random_walk_basic.py
@@ -1,15 +1,18 @@
 def main():
+    # Imports
     import matplotlib.pyplot as plt
     import random
+    # Variables called
     x_pos = 0
     y_pos = 0
     x = [0]
     y = [0]
     z = [zip(x, y)]
     a = 101
+    # For loop, gets random number and goes in direction based on that 
     for b in range(a):
         plt.plot(x, y, 'o-')
-        rand = random.randint(0,3)
+        rand = random.choice(0,1,2,3)
         if rand == 0:
             x_pos-=1
             x.append(x_pos)
@@ -26,6 +29,7 @@ def main():
             y_pos+=1
             x.append(x_pos)
             y.append(y_pos)
+    # Exports graph
     plt.xlabel("X-axis")
     plt.ylabel("Y-axis")
     plt.title("Random Walk")
